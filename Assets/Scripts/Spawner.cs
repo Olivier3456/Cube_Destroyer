524c095fa4aaf9974b1cd22616eb51f5bdd06e78
Olivier3456 <olivier.larrey@gmail.com>
2023-01-09 16:06:21 +0100

    first ex OK

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private Camera _cam;
+    [SerializeField] GameObject _prefabToSpawn;
+
+    [SerializeField] private float _distanceMin;
+    [SerializeField] private float _distanceMax;
+    [SerializeField] private float _spawnInterval;
+
+    private float _time;
+
+
+    void Start()
+    {
+
+    }
+
+    private void SpawnNewObject()
+    {
+        float randomDistance = Random.Range(_distanceMin, _distanceMax);
+
+        Vector3 positionObjectToSpawn = _cam.ScreenToWorldPoint(
+                new Vector3(Random.Range(0, Screen.width), Random.Range(0, Screen.height), randomDistance));
+        Instantiate(_prefabToSpawn, positionObjectToSpawn, Quaternion.identity);
+    }
+
+    void Update()
+    {
+        _time += Time.deltaTime;
+
+        if (_time >= _spawnInterval)
+        {
+            SpawnNewObject();
+            _time = 0;
+        }
+    }
+}
